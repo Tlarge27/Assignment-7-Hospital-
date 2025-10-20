87cc90afc5b0aa6a7abc8b8198f09c1504f4b6b7
Tlarge27 <157741770+Tlarge27@users.noreply.github.com>
2025-10-20 00:46:22 +0000

    Design Memo is on both files

--- doctor_tree.py
+++ doctor_tree.py
@@ -1,12 +1,97 @@
 class DoctorNode:
-    pass # Delete this line and implement the DoctorNode class
-
+    def __init__(self, name):
+        self.name = name
+        self.left = None
+        self.right = None
 
 
 class DoctorTree:
-    pass # Delete this line and implement the DoctorTree class
+    def __init__(self):
+        self.root = None
+
+    def insert(self, parent_name, employee_name, side):
+        new_node = DoctorNode(employee_name)
+        if self.root is None:
+            if parent_name == employee_name:
+                self.root = new_node
+                return True
+            else:
+                return False
+
+        parent_node = self._find(self.root, parent_name)
+        if parent_node is None:
+            return False
+
+        if side.lower() == "left":
+            if parent_node.left is None:
+                parent_node.left = new_node
+                return True
+            else:
+                return False
+        elif side.lower() == "right":
+            if parent_node.right is None:
+                parent_node.right = new_node
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def _find(self, node, name):
+        if node is None:
+            return None
+        if node.name == name:
+            return node
+        left_search = self._find(node.left, name)
+        if left_search:
+            return left_search
+        return self._find(node.right, name)
+
+    def preorder(self, node):
+        if node is None:
+            return []
+        return [node.name] + self.preorder(node.left) + self.preorder(node.right)
+
+    def inorder(self, node):
+        if node is None:
+            return []
+        return self.inorder(node.left) + [node.name] + self.inorder(node.right)
+
+    def postorder(self, node):
+        if node is None:
+            return []
+        return self.postorder(node.left) + self.postorder(node.right) + [node.name]
 
 
+if __name__ == "__main__":
+    tree = DoctorTree()
+    tree.root = DoctorNode("Dr. Croft")
+    tree.insert("Dr. Croft", "Dr. Goldsmith", "right")
+    tree.insert("Dr. Croft", "Dr. Phan", "left")
+    tree.insert("Dr. Phan", "Dr. Carson", "right")
+    tree.insert("Dr. Phan", "Dr. Morgan", "left")
 
+    print("Preorder:", tree.preorder(tree.root))
+    print("Inorder:", tree.inorder(tree.root))
+    print("Postorder:", tree.postorder(tree.root))
 
-# Test your DoctorTree and DoctorNode classes here
+# The tree created here is a binary tree that shows which doctors report to who and who is ultimately in charge.
+# Each Doctor is a Node named after the doctor and can have up to two people reporting to them. One on their left and one on their right.
+# The Tree class manages the structure and adds new doctors under whatever patient is specified using inserts. The tree is useful because 
+# it clearly shows who is in charge and who reports to who. Higher ranking doctors are on top with newer lower ranking doctors on the bottom.
+# Using a tree makes it easy to find current doctors, and it's easy to add new doctors. 
+#
+# There is three ways to work through this tree. There is Preorder, inorder, and postorder. 
+# The preorder goes to the main doctor first, then its left reports, then its right report. 
+# The inorder goes to the doctors that are on the left of the tree underneath the main doctor, starting from lowest to highest. 
+# It then goes to the main doctor, then to the doctor on the right that reports to the main doctor. 
+
+# A software engineer would use preorder when they need to see the top item first and then go through everything underneath it. 
+# they'd use inorder when they want to see the tree in order, like from smallest to biggest or shortest to tallest.
+# they would most likely use postorder when making edits to a tree. Like if a boss were to fire an employee their name would be removed  
+# from the bottom of the tree
+# 
+#
+#
+#
+#
--- emergency_queue.py
+++ emergency_queue.py
@@ -1,12 +1,86 @@
 class Patient:
-    pass # Delete this line and implement the Patient class
+    def __init__(self, name, urgency):
+        self.name = name 
+        self.urgency = urgency
 
-
+    def __repr__(self):
+        return f"{self.name} ({self.urgency})"
+    
 
 class MinHeap:
-    pass # Delete this line and implement the MinHeap class
+    def __init__(self):
+        self.data = []
+
+    def insert(self, patient):
+        self.data.append(patient)
+        self.heapify_up(len(self.data) - 1)
+
+    def heapify_up(self, index):
+        parent_index = (index - 1) // 2
+        if index > 0 and self.data[index].urgency < self.data[parent_index].urgency:
+            self.data[index], self.data[parent_index] = self.data[parent_index], self.data[index]
+            self.heapify_up(parent_index)
+
+    def heapify_down(self, index):
+        left = 2 * index + 1
+        right = 2 * index + 2
+        smallest = index
+
+        if left < len(self.data) and self.data[left].urgency < self.data[smallest].urgency:
+            smallest = left
+        if right < len(self.data) and self.data[right].urgency < self.data[smallest].urgency:
+            smallest = right
+
+        if smallest != index:
+            self.data[index], self.data[smallest] = self.data[smallest], self.data[index]
+            self.heapify_down(smallest)
+
+    def peek(self):
+        if not self.data:
+            return None
+        return self.data[0]
+
+    def remove_min(self):
+        if not self.data:
+            return None
+        min_patient = self.data[0]
+        last_patient = self.data.pop()
+        if self.data:
+            self.data[0] = last_patient
+            self.heapify_down(0)
+        return min_patient
+
+    def print_heap(self):
+        print("Current Queue:")
+        for patient in self.data:
+            print(f"- {patient.name} ({patient.urgency})")
 
 
+if __name__ == "__main__":
+    heap = MinHeap()
+    heap.insert(Patient("Jordan", 3))
+    heap.insert(Patient("Taylor", 1))
+    heap.insert(Patient("Avery", 5))
+    heap.insert(Patient("Riley", 2))
 
+    heap.print_heap()
+    print("\nNext up:", heap.peek())
 
-# Test your MinHeap class here including edge cases
+    served = heap.remove_min()
+    print("\nServed:", served.name)
+    heap.print_heap()
+
+# This Queue is a min heap that manages a list of patients based on the urgency there is to see them. 
+# Each patient is named and given a score from 1-10 based on how urgent their case is. the lower the urgency number the higher they 
+# are placed on the list, meaning a score of 10 would be least urgant and a score of 1 being the most urgent. 
+# The use of the Min heap here is to always keep the patient who is most urgent at the top. 
+# When a new patient arrives they are inserted into the pool of patients the heap pulls from. 
+# When a patient has been treated you can use remove_min to take them off the top of the list 
+# You are also able to view the entire list by viewing the heap or just the patient that is next in line by using peek. 
+# 
+# Heaps help real time systems like an emergency room intake by always keeping the most important thing on a to do list first. 
+#
+#
+#
+#
+#
